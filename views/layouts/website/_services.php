516e7fbe68b993a059db581cf6553edd9dc9f3fc
vishal-bhosle <vishal@elitefit.ai>
2021-07-19 00:06:21 +0530

    update
    Signed-off-by: vishal-bhosle <vishal@elitefit.ai>

--- views/layouts/website/_services.php
+++ views/layouts/website/_services.php
@@ -5,29 +5,32 @@
 
         <div class="section-title" data-aos="fade-up">
             <h2>Services</h2>
-            <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+            <p>Manasa is one of the licensed rehabilitation and DE-addiction facility situated
+                currently at Titwala providing comprehensive and holistic neuropsychiatric care under the able
+                guidance of psychiatrist and neurologist on 24/7 basis.</p>
         </div>
 
         <div class="row">
             <div class="col-lg-4 col-md-6" data-aos="fade-up">
                 <div class="icon-box">
                     <div class="icon"><i class="icofont-computer"></i></div>
-                    <h4 class="title"><a href="">Lorem Ipsum</a></h4>
-                    <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
+                    <h4 class="title"><a href="">Detoxification and Deaddiction</a></h4>
+                    <p class="description">Detoxification and Deaddiction ( alcohol, tobacco, charas, bhang, ganja, heroin,
+                        cocaine, LSD, meth, brown sugar, inhalants etc.</p>
                 </div>
             </div>
             <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
                 <div class="icon-box">
                     <div class="icon"><i class="icofont-chart-bar-graph"></i></div>
-                    <h4 class="title"><a href="">Dolor Sitema</a></h4>
-                    <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata</p>
+                    <h4 class="title"><a href="">Rehabilitation & NEUROREHABILITATION (DAY CARE)</a></h4>
+                    <p class="description">Rehabilitation for chronic psychiatric patients</p>
                 </div>
             </div>
             <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
                 <div class="icon-box">
                     <div class="icon"><i class="icofont-earth"></i></div>
-                    <h4 class="title"><a href="">Sed ut perspiciatis</a></h4>
-                    <p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+                    <h4 class="title"><a href="">Dementia care and rehabilitation</a></h4>
+                    <p class="description">Dementia care and rehabilitation (MEMORY AND COGNITIVE SKILL ENHANCEMENT FOR INDEPENDENT ACTIVITIES OF DAILY LIVING)</p>
                 </div>
             </div>
             <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="300">
@@ -40,15 +43,15 @@
             <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="400">
                 <div class="icon-box">
                     <div class="icon"><i class="icofont-settings"></i></div>
-                    <h4 class="title"><a href="">Nemo Enim</a></h4>
+                    <h4 class="title"><a href="">Old age rehabilitation</a></h4>
                     <p class="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque</p>
                 </div>
             </div>
             <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="500">
                 <div class="icon-box">
                     <div class="icon"><i class="icofont-tasks-alt"></i></div>
-                    <h4 class="title"><a href="">Eiusmod Tempor</a></h4>
-                    <p class="description">Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi</p>
+                    <h4 class="title"><a href="">Vocational Training</a></h4>
+                    <p class="description">vocational training to restore functionality on long term</p>
                 </div>
             </div>
         </div>
