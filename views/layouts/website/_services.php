38e2dd0f0efe48356f3fe58750a7bbc923d707e1
vishal-bhosle <vishal@elitefit.ai>
2021-10-08 01:47:19 +0530

    Services icons updated
    Signed-off-by: vishal-bhosle <vishal@elitefit.ai>

--- views/layouts/website/_services.php
+++ views/layouts/website/_services.php
@@ -11,7 +11,7 @@
         <div class="row">
             <div class="col-lg-4 col-md-6" data-aos="fade-up">
                 <div class="icon-box">
-                    <div class="icon"><i class="icofont-computer"></i></div>
+                    <div class="icon"><img src="https://i.ibb.co/SvRb6Nr/tea-bag.png" alt="tea-bag" border="0"></div>
                     <h4 class="title"><a href="">Detoxification and Deaddiction</a></h4>
                     <p class="description">Detoxification and Deaddiction ( alcohol, tobacco, charas, bhang, ganja, heroin,
                         cocaine, LSD, meth, brown sugar, inhalants etc.</p>
@@ -19,35 +19,35 @@
             </div>
             <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
                 <div class="icon-box">
-                    <div class="icon"><i class="icofont-chart-bar-graph"></i></div>
+                    <div class="icon"><img src="https://i.ibb.co/DCXQR7x/care.png" alt="care" border="0"></div>
                     <h4 class="title"><a href="">Rehabilitation & NEUROREHABILITATION (DAY CARE)</a></h4>
                     <p class="description">Enhancing neuromuscular functioning and cognitive skills for independent Activities of daily living.</p>
                 </div>
             </div>
             <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
                 <div class="icon-box">
-                    <div class="icon"><i class="icofont-earth"></i></div>
+                    <div class="icon"><img src="https://i.ibb.co/58mNd52/rehabilitation.png" alt="rehabilitation" border="0"></div>
                     <h4 class="title"><a href="">Dementia care and rehabilitation</a></h4>
                     <p class="description">Dementia care and rehabilitation (memory and cognitive skill enhancement for independent activities of daily living).</p>
                 </div>
             </div>
             <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="300">
                 <div class="icon-box">
-                    <div class="icon"><i class="icofont-image"></i></div>
+                    <div class="icon"><img src="https://i.ibb.co/wJDP0z3/massage.png" alt="massage" border="0"></div>
                     <h4 class="title"><a href="">Psychiatric rehabilitation in place of</a></h4>
                     <p class="description">Functional recovery in chronic psychiatric patients through psychosocial rehabilitation.</p>
                 </div>
             </div>
             <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="400">
                 <div class="icon-box">
-                    <div class="icon"><i class="icofont-settings"></i></div>
+                    <div class="icon"><img src="https://i.ibb.co/0cBPnT4/nursing-home.png" alt="nursing-home" border="0"></div>
                     <h4 class="title"><a href="">Old age rehabilitation</a></h4>
                     <p class="description">Promoting healthy aging through cognitive skill enhancement and active participation in self-help skills.</p>
                 </div>
             </div>
             <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="500">
                 <div class="icon-box">
-                    <div class="icon"><i class="icofont-tasks-alt"></i></div>
+                    <div class="icon"><img src="https://i.ibb.co/smG50P8/presentation.png" alt="presentation" border="0"></div>
                     <h4 class="title"><a href="">Vocational Training</a></h4>
                     <p class="description">To restore functionality through motivation enhancement, personality development, and life skill training.</p>
                 </div>
